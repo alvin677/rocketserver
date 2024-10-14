ffffaaa1bb3369c5cd0514520873eacf87595db1
GoatCedric <112005397+GoatCedric@users.noreply.github.com>
2024-10-14 15:29:46 +0200

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use rocket::fs::{FileServer, relative};
+use std::fs;
+
+#[macro_use] extern crate rocket;
+
+// index.html file in directory
+#[get("/")]
+fn index() -> rocket::response::content::RawHtml<String> {
+    return rocket::response::content::RawHtml(fs::read_to_string("./html/index.html").unwrap());
+}
+
+// any other file in the directory
+#[get("/<file>")]
+fn site(file: &str) -> rocket::response::content::RawHtml<String> {
+    let index_file: String;
+    if fs::exists(format!("./html/{}.html", file)).unwrap() {
+        index_file = fs::read_to_string(format!("./html/{}.html", file)).unwrap();
+    } else {
+        index_file = fs::read_to_string("./html/err.html").unwrap();
+    }
+    return rocket::response::content::RawHtml(index_file);
+}
+
+// api test
+#[get("/hello/<name>/<age>")]
+fn hello(name: &str, age: u8) -> String {
+    format!("Hello, {} year old named {}!", age, name)
+}
+
+// define routes to use
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![index, hello, site])
+        .mount("/html", FileServer::from(relative!("html")))
+}
